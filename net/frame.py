bed1fecd6b62f8144c818d5567daf8aaa43a1aab
Yann <yann.pomie@gmail.com>
2022-01-31 10:50:00 +0100

    inherits DOSFrame

--- net/frame.py
+++ net/frame.py
@@ -18,18 +18,32 @@ class Tab(DOSMFrame):
         self.keys = []
         for key in self.interfaces.keys():
             self.keys.append(key)
-        self.varAdress = StringVar()
+
+        print(self.interfaces)
+
+        self.varStats = StringVar()
+        self.varAddress = StringVar()
         self.selected = self.keys[0]
 
     def show(self):
-        comboBox = ttk.Combobox(self, justify="left", height=10, state="readonly", values=self.keys)
+        labelselect = ttk.Label(self, justify="center", text="Please select an interface")
+        labelselect.grid(row=0, column=0)
+
+        comboBox = ttk.Combobox(self, justify="center", height=10, state="readonly", values=self.keys)
         comboBox.bind('<<ComboboxSelected>>', func=self.changeSelected)
-        comboBox.pack(side=LEFT)
-        ipaddress = ttk.Label(self, justify="left", textvariable=self.varAdress)
-        ipaddress.pack(side=LEFT)
+        comboBox.grid(row=1, column=0)
+
+        ipaddress = ttk.Label(self, justify="center", textvariable=self.varAddress)
+        ipaddress.grid(row=2, column=0)
+
+        labelstats = ttk.Label(self, justify="center", text="Stats")
+        labelstats.grid(row=3, column=0)
+
+        stats = ttk.Label(self, justify="center", textvariable=self.varStats)
+        stats.grid(row=4, column=0)
 
     def update(self, dt):
-        return super().update(dt)
+        print(dt)
 
     def hide(self):
         return super().hide()
@@ -37,7 +51,12 @@ class Tab(DOSMFrame):
     def changeSelected(self, event):
         if event:
             self.selected = event.widget.get()
-            self.varAdress.set(str(self.getIpAddress(self.selected)))
+            self.varAddress.set(str(self.getIpAddress(self.selected)))
+            self.varStats.set(str(self.getStats(self.selected)))
 
     def getIpAddress(self, interface):
         return self.interfaces.get(interface)[0].__getattribute__('address')
+
+    def getStats(self, interface):
+        return f"Netmask : {self.interfaces.get(interface)[0].__getattribute__('netmask')}\n"\
+               + f"Broadcast address : {self.interfaces.get(interface)[0].__getattribute__('broadcast')}"
