f20b3b09edc4ffc886cc06f3ba7acc71a120250d
Nils Ponsard <nilsponsard@gmail.com>
2022-01-31 11:11:03 +0100

    read_connexions

--- ports/read.py
+++ ports/read.py
@@ -2,25 +2,66 @@ import os
 import psutil
 
 
-def readProcs():
-    '''
-    Lists the /proc/[pid] folders and returns a list of processes
-    '''
-    procs = []
-    for pid in os.listdir('/proc'):
-        if pid.isdigit():
-            procs.append(pid)
-    return procs
+def read_connexions():
+    """
+    Reads all inet connexions on the system.
+    Returns a list of connexions with the following structure:
+    [
+        {
+            'pid': 1,
+            'name': "",
+            'local_addr': "",
+            'local_port': "",
+            'remote_addr': "",
+            'remote_port': "",
+            'status': "",
+            'type': < SOCK_STREAM | SOCK_DGRAM | SOCK_SEQPACKET >
+        },
+        ...
+    ]
+    """
+    connexions = psutil.net_connections()
+
+    out = []
+
+    for con in connexions:
+
+        name = "unknown"
+
+        if (con.pid is not None):
+            try:
+                name = psutil.Process(con.pid).name()
+            except:
+                pass
+
+        local_addr = "unknown"
+        local_port = ""
+        if (con.laddr != ()):
+            local_addr = con.laddr.ip
+            local_port = str(con.laddr.port)
+
+        remote_addr = ""
+        remote_port = ""
+        if (con.raddr != ()):
+            remote_addr = con.raddr.ip
+            remote_port = str(con.raddr.port)
+
+        out.append({
+            'pid': con.pid,
+            'name': name,
+            'local_addr': local_addr,
+            'local_port': local_port,
+            'remote_addr': remote_addr,
+            'remote_port': remote_port,
+            'status': con.status,
+            'type': con.type
+        })
+    return out
 
 
 if __name__ == '__main__':
 
-    connextions = psutil.net_connections()
-
-    for con in connextions:
-        print(con.pid,con.laddr)
-
-
+    print(read_connexions())
 
     # print(psutil.net_connections())
     # print(readProcs())
