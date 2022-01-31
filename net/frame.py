8e177d81389ea184cdf962fc67233734ea75762c
Yann <yann.pomie@gmail.com>
2022-01-31 10:50:00 +0100

    inherits DOSFrame

--- net/frame.py
+++ net/frame.py
@@ -1,15 +1,7 @@
 from integrator.frame import DOSMFrame
 from tkinter import *
 from tkinter import ttk
-
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import psutil
-
-#TODO
-#   -combo list with interfaces
-#   -draw switchband usage and find a library to do so..
-
 
 class Tab(DOSMFrame):
     def __init__(self, master, logger, **options):
@@ -18,11 +10,22 @@ class Tab(DOSMFrame):
         self.interfaces = psutil.net_if_addrs()
         self.interfaces.pop('lo')
 
-        self.if_stats = psutil.net_if_stats()
+        self.if_stats = dict(psutil.net_if_stats())
         self.if_stats.pop('lo')
+
+        self.if_counters = dict(psutil.net_io_counters(pernic=True))
+        self.if_counters.pop('lo')
+
+        self.selected = None
 
         self.varStats = StringVar()
         self.varAddress = StringVar()
+
+        self.varRecv = StringVar()
+        self.statRecv = StringVar()
+
+        self.varSent = StringVar()
+        self.statSent = StringVar()
 
     def show(self):
         labelselect = ttk.Label(self, justify="center", text="Please select an interface")
@@ -43,12 +46,22 @@ class Tab(DOSMFrame):
         stats = ttk.Label(self, justify="center", textvariable=self.varStats)
         stats.grid(row=4, column=0)
 
-        figure = plt.figure(figsize=(6, 5), dpi=100)
-        diagram = FigureCanvasTkAgg(figure=figure, master=self)
-        diagram.get_tk_widget().grid(column=1)
+        bytesrecv = ttk.Label(self, justify="center", textvariable=self.varRecv)
+        bytesrecv.grid(row=1, column=1)
+        statsrecv = ttk.Label(self, justify="center", textvariable=self.statRecv)
+        statsrecv.grid(row=2, column=1)
+
+        bytessent = ttk.Label(self, justify="center", textvariable=self.varSent)
+        bytessent.grid(row=1, column=2)
+        statssent = ttk.Label(self, justify="center", textvariable=self.statSent)
+        statssent.grid(row=2, column=2)
+
+        self.animate(self.selected)
 
     def update(self, dt):
-        return super().update(dt)
+        self.animate(self.selected)
+
+        #log here
 
     def hide(self):
         return super().hide()
@@ -59,6 +72,13 @@ class Tab(DOSMFrame):
             self.varAddress.set(str(self.getIpAddress(self.selected)))
             self.varStats.set(str(self.getStats(self.selected)))
 
+            self.varSent.set(f"{self.if_counters.get(self.selected).__getattribute__('bytes_sent')} bytes send")
+            self.statSent.set(f"packets sent {self.if_counters.get(self.selected).__getattribute__('packets_sent')}")
+
+            self.varRecv.set(f"{self.if_counters.get(self.selected).__getattribute__('bytes_recv')} bytes received")
+            self.statRecv.set(f"packets received {self.if_counters.get(self.selected).__getattribute__('packets_recv')}")
+
+
     def getIpAddress(self, interface):
         return self.interfaces.get(interface)[0].__getattribute__('address')
 
@@ -67,12 +87,9 @@ class Tab(DOSMFrame):
                + f"Broadcast address : {self.interfaces.get(interface)[0].__getattribute__('broadcast')}"
 
     def updateValues(self):
-        self.if_stats = psutil.net_if_stats()
+        self.if_counters = dict(psutil.net_io_counters(pernic=True))
+        self.if_counters.pop('lo')
 
-    def animate(self, interface):
+    def animate(self, selected):
         self.updateValues()
-        #TODO
-        #foreach val
-            #print into matplotlib
-        #clear
         pass
