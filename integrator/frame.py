4a0200dceb971c4f374c7ca028e236a3f6a01b33
Julien Dubois <dubois.julien.mail@gmail.com>
2022-01-31 10:32:59 +0100

    [integrator] fix DOSMFrame __init__

--- integrator/frame.py
+++ integrator/frame.py
@@ -2,8 +2,9 @@ import tkinter as tk
 
 
 class DOSMFrame(tk.Frame):
-    def __init__(self, logger):
+    def __init__(self, master, logger, **options):
         self.logger = logger
+        super().__init__(master, **options)
 
     def show(self):
         pass
