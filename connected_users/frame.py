d2494c6fe0765522a1a0b3d872a44cb5bd0272a9
Alexis-Bernard <alexisbernard34310@gmail.com>
2022-01-31 10:20:14 +0100

    Add __init__.py file

--- connected_users/frame.py
+++ connected_users/frame.py
@@ -1,34 +1,60 @@
-import psutil
+import psutil as ps
 import tkinter as tk
+from tkinter import ttk
 from integrator import frame
+from logger.logger import Logger
+from datetime import datetime
 
 
 class frame(frame.DOSMFrame):
-    def __init__(self, master, logger, **options):
+    def __init__(self, master, logger: Logger, **options):
         self.logger = logger
-        self.master = master
 
-        super().__init__(master, logger, **options)
+        master.columnconfigure(0, weight=1)
+        master.rowconfigure(0, weight=1)
+
         master.title("Logged in users")
 
-        self.greet_button = tk.Button(master, text="Refresh", command=self.getConnectedUsers)
-        self.greet_button.pack()
+        # User columns name and heading text
+        columns = {
+            "name": "Name",
+            "host": "Host",
+            "started": "Login date",
+            "connexion_pid": "Connexion PID"
+        }
+
+        self.datagrid = ttk.Treeview(
+            master, columns=list(columns.keys()), show='headings')
+
+        # Set headings
+        for column in columns.keys():
+            self.datagrid.heading(column, text=columns[column])
+
+        self.datagrid.grid(row=0, column=0, sticky='nsew')
+
+        # Scrollbar
+        scrollbar = ttk.Scrollbar(
+            master, orient=tk.VERTICAL, command=self.datagrid.yview)
+        self.datagrid.configure(yscroll=scrollbar.set)
+        scrollbar.grid(row=0, column=1, sticky='ns')
 
     def show(self):
-        pass
+        self.update(0)
 
     def update(self, dt):
-        """
-        `dt` is the elapsed delta time since the last update in second
-        """
-        pass
+        # Clear data grid
+        for item in self.datagrid.get_children():
+            self.datagrid.delete(item)
+
+        users = []
+        for user in ps.users():
+            users.append(
+                (user.name, user.host, datetime.fromtimestamp(user.started), user.pid))
+            self.logger.write_log(users[len(users) - 1])
+
+        # Add data to datagrid
+        for user in users:
+            self.datagrid.insert('', tk.END, values=user)
 
     def hide(self):
         pass
-
-    def getConnectedUsers(self):
-        users = psutil.users()
-
-        print(len(users))
-        print(users)
-        
