be49fb5b4acb7f57be0526ca9d5392faa7e5cbf6
Yann <yann.pomie@gmail.com>
2022-01-31 10:50:00 +0100

    inherits DOSFrame

--- net/frame.py
+++ net/frame.py
@@ -1,11 +1,28 @@
 from integrator.frame import DOSMFrame
+from tkinter import *
+from tkinter import ttk
+import psutil
+
+#TODO
+#   -combo list with interfaces
+#   -draw switchband usage and find a library to do so..
 
 class Tab(DOSMFrame):
-    def __init__(self, logger):
-        print("some cool logger passing to super")
+    def __init__(self, master, logger, **options):
+        super(Tab, self).__init__(master, logger, **options)
+
+        self.interfaces = psutil.net_if_addrs()
+        self.interfaces.pop('lo')
+
+        self.keys = []
+        for key in self.interfaces.keys():
+            self.keys.append(key)
 
     def show(self):
-        return super().show()
+        comboBox = ttk.Combobox(self, justify="left",
+                                height=10, state="normal", values=self.keys)
+        comboBox.pack(side=LEFT)
+
 
     def update(self, dt):
         return super().update(dt)
