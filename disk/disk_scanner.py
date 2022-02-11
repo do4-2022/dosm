fa0e989725a677a7cbe6a0e6c0f63bc753f1057d
Mathieu Soysal <Mathieu.Soysal@etu.umontpellier.fr>
2022-02-11 20:45:53 +0100

    Update disk_canner

--- disk/disk_scanner.py
+++ disk/disk_scanner.py
@@ -1,4 +1,5 @@
-from disk import partition
+from disk import Partition
+from disk import DiskUsage
 
 def get_partitions():
     """
@@ -9,10 +10,34 @@ def get_partitions():
         if line.startswith('major') or line.startswith('\n'):
             continue
         fields = line.split()
-        partitions.append(partition(
+        partitions.append(Partition(
             int(fields[0]),
             int(fields[1]),
             int(fields[2]),
             fields[3]
         ))
     return partitions
+
+def get_disk_usage():
+    """
+    This function returns a list of disk usage.
+    """
+    disk_usages = []
+    for line in open('/proc/diskstats'):
+        fields = line.split()
+        disk_usages.append(DiskUsage(
+            int(fields[0]),
+            int(fields[1]),
+            fields[2],
+            int(fields[3]),
+            int(fields[4]),
+            int(fields[5]),
+            int(fields[6]),
+            int(fields[7]),
+            int(fields[8]),
+            int(fields[9]),
+            int(fields[10]),
+            int(fields[11]),
+            int(fields[12]),
+            int(fields[13])))
+    return disk_usages
