3226513489b2c294e979a2fce0d0181ed19117ff
Alexis-Bernard <alexisbernard34310@gmail.com>
2022-02-11 19:00:23 +0100

    feat: Connected users

--- connected_users/frame.py
+++ connected_users/frame.py
@@ -1,7 +1,19 @@
+import psutil
 import tkinter as tk
 from integrator import frame
 
-class Tab(frame.DOSMFrame):
+
+class frame(frame.DOSMFrame):
+    def __init__(self, master, logger, **options):
+        self.logger = logger
+        self.master = master
+
+        super().__init__(master, logger, **options)
+        master.title("Logged in users")
+
+        self.greet_button = tk.Button(master, text="Refresh", command=self.getConnectedUsers)
+        self.greet_button.pack()
+
     def show(self):
         pass
 
@@ -13,3 +25,10 @@ class Tab(frame.DOSMFrame):
 
     def hide(self):
         pass
+
+    def getConnectedUsers(self):
+        users = psutil.users()
+
+        print(len(users))
+        print(users)
+        
