c117a62b1ca2d4d5ba54ba68d647cae21c475bb2
Simon Lucido <simon34280@gmail.com>
2022-02-11 18:00:28 +0100

    feat: computing total shared memory usage

--- IPC/frame.py
+++ IPC/frame.py
@@ -2,18 +2,20 @@ import os
 import subprocess
 import tkinter as tk
 from integrator import frame  
-import psutil
 
 class Tab(frame.DOSMFrame):
     counter = 0
 
     def __init__(self, master, logger, **options):
+        self.pipes = {}
+        self.shared_memory = 0
         super().__init__(master, logger, **options)
 
     def show(self):
         self.label = tk.Label(self, text="IPC Works! {}".format(self.counter) )
         self.label.pack()
-        self.load_lsof();
+        self.load_shared_memory()
+        self.load_pipes()
 
     def update(self, dt):
         """
@@ -25,17 +27,25 @@ class Tab(frame.DOSMFrame):
     def hide(self):
         pass
 
-    def load_lsof(self):
-        dictionary = {};
+    def load_pipes(self):
+        self.pipes = {};
         lsof = subprocess.Popen(["lsof"], stdout=subprocess.PIPE)
         grep = subprocess.Popen(["grep", "FIFO"], stdin=lsof.stdout, stdout=subprocess.PIPE)
         awk = subprocess.Popen(["awk", "{print $1}"], stdin=grep.stdout, stdout=subprocess.PIPE)
         for line in awk.stdout.readlines():
-            number = dictionary.get(line.decode().strip('\n'))
+            number = self.pipes.get(line.decode().strip('\n'))
             if number == None:
-                dictionary[line.decode().strip('\n')] = 1
+                self.pipes[line.decode().strip('\n')] = 1
             else:
-                dictionary[line.decode().strip('\n')] = number + 1
+                self.pipes[line.decode().strip('\n')] = number + 1
             
-        print(dictionary)
-        #stdout = os.system("")
+    def load_shared_memory(self):
+        self.shared_memory = 0
+        shm = subprocess.Popen(["ipcs", "-m"], stdout=subprocess.PIPE)
+        awk = subprocess.Popen(["awk", "NR > 3 { print $5 }"] ,stdin=shm.stdout, stdout=subprocess.PIPE)
+        for line in awk.stdout.readlines():
+            decoded = line.decode().strip('\n')
+            if decoded.isnumeric():
+                self.shared_memory += int(decoded)
+        print(self.shared_memory / 1000000)
+
