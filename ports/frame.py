2e5bdb287f34454e55be7209601cebf033458ffa
Nils Ponsard <nilsponsard@gmail.com>
2022-02-11 15:16:37 +0100

    test

--- ports/frame.py
+++ ports/frame.py
@@ -1,8 +1,14 @@
 from integrator import frame
+import tkinter as tk
+
 
 class PortTab (frame.DOSMFrame):
     def __init__(self, master, logger, **options):
         super().__init__(master, logger, **options)
 
+
+    def show(self):
+        bluebutton = tk.Button(self, text="Blue", fg="blue")
+        bluebutton.pack( side = tk.LEFT )
 
         
