938ed875c1ecb83311ba8f0aa9e333b6c67e7f0e
WoodenMaiden <yann.pomie@etu.umontpellier.fr>
2022-02-11 17:52:55 +0100

    get the adress of interface, TODO check if IPV4

--- net/frame.py
+++ net/frame.py
@@ -18,14 +18,31 @@ class Tab(DOSMFrame):
         self.keys = []
         for key in self.interfaces.keys():
             self.keys.append(key)
+        self.selected = self.keys[0]
 
     def show(self):
-        comboBox = ttk.Combobox(self, justify="left",
-                                height=10, state="normal", values=self.keys)
+        comboBox = ttk.Combobox(self, justify="left", height=10, state="readonly", values=self.keys)
+        comboBox.bind('<<ComboboxSelected>>', func=self.changeSelected)
         comboBox.pack(side=LEFT)
+
+        varAdress = StringVar()
+
+        # TODO
+        #varAdress.set(self.getIpAddress(self.selected))
+
+        ipaddress = ttk.Label(self, justify="left", textvariable=varAdress)
+        ipaddress.pack(side=LEFT)
 
     def update(self, dt):
         return super().update(dt)
 
     def hide(self):
         return super().hide()
+
+    def changeSelected(self, event):
+        if event:
+            self.selected = event.widget.get()
+            print(event.widget.get())
+
+    def getIpAddress(self, interface):
+        return self.interfaces.get(interface)[0].__getattribute__('address')
