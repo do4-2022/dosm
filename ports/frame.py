dbdf675015ec7e94286687cbab8346004976f33c
Nils Ponsard <nilsponsard@gmail.com>
2022-02-11 16:36:40 +0100

    start

--- ports/frame.py
+++ ports/frame.py
@@ -3,7 +3,8 @@ import tkinter as tk
 from tkinter import ttk
 from ports import read
 
-header = ["port", "PID", "name"]
+columns = ["local port", "local addr", "remote addr",
+           "remote port", "PID", "name", "status", "type"]
 
 
 class PortTab (frame.DOSMFrame):
@@ -11,22 +12,30 @@ class PortTab (frame.DOSMFrame):
         super().__init__(master, logger, **options)
 
     def show(self):
+        tree = ttk.Treeview(self, columns=columns, show="headings", height=30)
+        for head in columns:
+            tree.heading(head, text=head)
 
-        scrollbar = ttk.Scrollbar(self)
-        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-        mylist = tk.Listbox(self, yscrollcommand=scrollbar.set)
-
+        #mylist = tk.Listbox(self, yscrollcommand=scrollbar.set)
 
         res = read.read_connexions()
 
-
         print(len(res))
         for item in res:
-            mylist.insert(tk.END, f"{item['local_port']} | {item['pid']} | {item['name']}")
+            tree.insert(
+                '', tk.END, values=(item['local_port'], item['local_addr'], item['remote_addr'], item['remote_port'], item['pid'], item['name'], item['status'], item['type']))
+        
+        
+        
+        tree.grid(row=0, column=0, sticky='nsew')
 
-        mylist.pack(side=tk.LEFT, fill=tk.BOTH)
-        scrollbar.config(command=mylist.yview)
+        scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL, command=tree.yview)
+        tree.configure(yscroll=scrollbar.set)
+        scrollbar.config(command=tree.yview)
+        scrollbar.grid(row=0, column=1, sticky='ns')
 
+        # scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        #
         # for i in range(len(header)):
         #     e = tk.Entry(self, font=("Arial", 12))
         #     e.grid(row=0, column=i)
