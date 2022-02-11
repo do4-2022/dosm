c3db5c2ef2286df7b9a10be809ecfc347ed63dfc
iverly <iverly.dev@gmail.com>
2022-02-11 17:48:47 +0100

    feat: split file and add version command

--- cli/main.py
+++ cli/main.py
@@ -1,17 +1,7 @@
+from logs import logs
+from misc import misc
 import click
 
-VERSION = "1.0.0"
-
-@click.group()
-def misc():
-  pass
-
-@misc.command()
-def version():
-  """Show the cli version and exit."""
-  click.echo(f"Cli version: {VERSION}")
-
-
-cli = click.CommandCollection(sources=[misc])
+cli = click.CommandCollection(sources=[logs, misc])
 if __name__ == '__main__':
   cli()
