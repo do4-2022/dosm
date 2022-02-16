bac065cb5ae123a8116cc0666026708a6695a6a3
Julien Dubois <dubois.julien.mail@gmail.com>
2022-02-16 21:13:43 +0100

    [integrator] integrate home tab

--- main.py
+++ main.py
@@ -4,34 +4,40 @@ from time import time
 import tkinter as tk
 from tkinter import ttk
 
-import config
+from config import UPDATE_INTERVAL, MIN_WINDOW_SIZE
+from home import frame as home_frame
+from logger import factory, logger
 
 
 def update(root_window, frames, last_time):
     new_time = time()
     for frame in frames:
         frame['widget'].update(new_time - last_time)
-    root_window.after(config.UPDATE_INTERVAL, update, root_window, frames, new_time)
+    root_window.after(UPDATE_INTERVAL, update, root_window, frames, new_time)
 
 
 def main():
     root_window = tk.Tk()
     root_window.title('DOSM')
-    root_window.minsize(*config.MIN_WINDOW_SIZE)
+    root_window.minsize(*MIN_WINDOW_SIZE)
 
     notebook = ttk.Notebook(root_window)
-    notebook.pack()
+    notebook.pack(fill='both', expand=True)
 
-    frames = []
+    logger_factory = factory.LoggerFactory()
 
-    if not frames:
-        error_label = tk.Label(notebook, text='No frame found')
-        error_label.pack()
+    frames = [
+        {
+            'name': 'Accueil',
+            'widget': home_frame.Tab(notebook, logger.Logger('home', logger_factory))
+        }
+    ]
 
     for frame in frames:
+        frame['widget'].show()
         notebook.add(frame['widget'], text=frame['name'])
 
-    root_window.after(config.UPDATE_INTERVAL, update, root_window, frames, time())
+    root_window.after(UPDATE_INTERVAL, update, root_window, frames, time())
     root_window.mainloop()
 
 
