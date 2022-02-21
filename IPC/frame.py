79a09ed7b39c1328b9d926062cc2bfcd7c2d6f79
Simon Lucido <simon34280@gmail.com>
2022-02-21 20:22:38 +0100

    refactor: moved data loading functions into another file

--- IPC/frame.py
+++ IPC/frame.py
@@ -1,6 +1,6 @@
-import subprocess
 import threading
 import tkinter as tk
+from IPC import utils
 from integrator import frame  
 from threading import Thread
 
@@ -11,7 +11,7 @@ class Tab(frame.DOSMFrame):
         self.pipes = {}
         self.shared_memory = 0
         self.semaphores = 0
-        Thread(None, self.load_pipes).start()
+        Thread(None, self.pipe_thread).start()
         super().__init__(master, logger, **options)
 
     def show(self):
@@ -22,12 +22,14 @@ class Tab(frame.DOSMFrame):
         self.set = tk.ttk.Treeview(self)
         self.set.pack()
 
-        self.set['columns']= ('process', 'pipe_number')
+        self.set['columns']= ('pid', 'process', 'pipe_number')
         self.set.column("#0", width=0,  stretch=tk.NO)
+        self.set.column("pid",anchor=tk.CENTER, width=50)
         self.set.column("process",anchor=tk.CENTER, width=150)
         self.set.column("pipe_number",anchor=tk.CENTER, width=100)
 
         self.set.heading("#0",text="",anchor=tk.CENTER)
+        self.set.heading("pid",text="PID",anchor=tk.CENTER)
         self.set.heading("process",text="Process",anchor=tk.CENTER)
         self.set.heading("pipe_number",text="# of pipes",anchor=tk.CENTER)
 
@@ -36,59 +38,30 @@ class Tab(frame.DOSMFrame):
 
 
     def update(self, dt):
-        self.load_shared_memory()
-        self.load_semaphores()
+        self.shared_memory = utils.load_shared_memory() 
+        self.semaphores = utils.load_semaphores()
         self.semaphore_label["text"] = "Number of semaphores : {}".format(self.semaphores)
-        self.shared_memory_label["text"] = "Total shared  memory usage : {} MB".format(self.shared_memory / 1000)
+        self.shared_memory_label["text"] = "Total shared  memory usage : {} MB".format(self.shared_memory / 1000000)
 
         if not self.working:
             self.working = True
-            for index, ( key, value ) in enumerate(self.pipes.items()):
+            for index, ( (pid, process), value ) in enumerate(self.pipes.items()):
                 if self.set.exists(index):
                     self.set.delete(index)
                 self.set.insert(parent='',index='end',iid=index, text='',
-                values=(key, value))
+                values=(pid, process, value))
 
             self.working = False
 
     def hide(self):
         pass
 
-    def load_pipes(self):
-        lsof = subprocess.Popen(["lsof"], stdout=subprocess.PIPE)
-        grep = subprocess.Popen(["grep", "FIFO"], stdin=lsof.stdout, stdout=subprocess.PIPE)
-        awk = subprocess.Popen(["awk", "{print $1}"], stdin=grep.stdout, stdout=subprocess.PIPE)
-
+    def pipe_thread(self):
         while self.working:
             pass
 
         self.working = True
-        self.pipes = {}
-        for line in awk.stdout.readlines():
-            number = self.pipes.get(line.decode().strip('\n'))
-            if number == None:
-                self.pipes[line.decode().strip('\n')] = 1
-            else:
-                self.pipes[line.decode().strip('\n')] = number + 1
-
+        self.pipes = utils.load_pipes()
         self.working = False
-
-        threading.Timer(30, self.load_pipes)
-            
-    def load_shared_memory(self):
-        self.shared_memory = 0
-        shm = subprocess.Popen(["ipcs", "-m"], stdout=subprocess.PIPE)
-        awk = subprocess.Popen(["awk", "NR > 3 { print $5 }"] ,stdin=shm.stdout, stdout=subprocess.PIPE)
-        for line in awk.stdout.readlines():
-            decoded = line.decode().strip('\n')
-            if decoded.isnumeric():
-                self.shared_memory += int(decoded)
-
-    def load_semaphores(self):
-        self.semaphores = 0
-        shm = subprocess.Popen(["ipcs", "-sc"], stdout=subprocess.PIPE)
-        awk = subprocess.Popen(["awk", "NR > 3 { print $4 }"] ,stdin=shm.stdout, stdout=subprocess.PIPE)
-        for line in awk.stdout.readlines():
-            decoded = line.decode().strip('\n')
-            if len(decoded) > 1:
-                self.semaphores += 1
+        threading.Timer(30, self.pipe_thread)
+   
