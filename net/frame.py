1ad2266ee50180e045a881a4f44b31a13675434d
WoodenMaiden <yann.pomie@etu.umontpellier.fr>
2022-02-22 17:41:09 +0100

    added components

--- net/frame.py
+++ net/frame.py
@@ -13,7 +13,7 @@ class Tab(DOSMFrame):
         self.if_stats = dict(psutil.net_if_stats())
         self.if_stats.pop('lo')
 
-        self.if_counters = dict(psutil.net_io_counters(pernic=True))
+        self.if_counters = dict(psutil.net_io_counters(pernic=True, nowrap=True))
         self.if_counters.pop('lo')
 
         self.selected = None
@@ -22,9 +22,13 @@ class Tab(DOSMFrame):
         self.varAddress = StringVar()
 
         self.varRecv = StringVar()
+        self.varRecv.set("0 bytes received")
+
         self.statRecv = StringVar()
 
         self.varSent = StringVar()
+        self.varSent.set("0 bytes sent")
+
         self.statSent = StringVar()
 
     def show(self):
@@ -46,20 +50,24 @@ class Tab(DOSMFrame):
         stats = ttk.Label(self, justify="center", textvariable=self.varStats)
         stats.grid(row=4, column=0)
 
+        iconrecv = ttk.Label(self, justify="center", text="🡇")
+        iconrecv.grid(row=0, column=1)
         bytesrecv = ttk.Label(self, justify="center", textvariable=self.varRecv)
         bytesrecv.grid(row=1, column=1)
         statsrecv = ttk.Label(self, justify="center", textvariable=self.statRecv)
         statsrecv.grid(row=2, column=1)
 
+        iconsent = ttk.Label(self, justify="center", text="🡅")
+        iconsent.grid(row=0, column=2)
         bytessent = ttk.Label(self, justify="center", textvariable=self.varSent)
         bytessent.grid(row=1, column=2)
         statssent = ttk.Label(self, justify="center", textvariable=self.statSent)
         statssent.grid(row=2, column=2)
 
-        self.animate(self.selected)
+        self.updateValues()
 
     def update(self, dt):
-        self.animate(self.selected)
+        self.updateValues()
 
         #log here
 
@@ -68,14 +76,15 @@ class Tab(DOSMFrame):
 
     def changeSelected(self, event):
         if event:
+            self.updateValues()
             self.selected = event.widget.get()
             self.varAddress.set(str(self.getIpAddress(self.selected)))
             self.varStats.set(str(self.getStats(self.selected)))
 
-            self.varSent.set(f"{self.if_counters.get(self.selected).__getattribute__('bytes_sent')} bytes send")
+            self.varSent.set(f"{self.prettyPrintBytes(self.if_counters.get(self.selected).__getattribute__('bytes_sent'))} send")
             self.statSent.set(f"packets sent {self.if_counters.get(self.selected).__getattribute__('packets_sent')}")
 
-            self.varRecv.set(f"{self.if_counters.get(self.selected).__getattribute__('bytes_recv')} bytes received")
+            self.varRecv.set(f"{self.prettyPrintBytes(self.if_counters.get(self.selected).__getattribute__('bytes_recv'))} received")
             self.statRecv.set(f"packets received {self.if_counters.get(self.selected).__getattribute__('packets_recv')}")
 
 
@@ -87,9 +96,12 @@ class Tab(DOSMFrame):
                + f"Broadcast address : {self.interfaces.get(interface)[0].__getattribute__('broadcast')}"
 
     def updateValues(self):
-        self.if_counters = dict(psutil.net_io_counters(pernic=True))
+        self.if_counters = dict(psutil.net_io_counters(pernic=True, nowrap=True))
         self.if_counters.pop('lo')
 
-    def animate(self, selected):
-        self.updateValues()
-        pass
+    def prettyPrintBytes(self, bytes_amount):
+        #TODO
+        sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+        index = 0
+        amount = 0
+        return f"{amount} {sizes[index]}"
