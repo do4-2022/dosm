2a41b98310f450cfd51160d09b4371b0cdb84c89
esteban <esteban.baron@etu.umontpellier.fr>
2022-02-27 23:29:54 +0100

    Integrate memory summary frame

--- memory/summary_frame.py
+++ memory/summary_frame.py
@@ -1,25 +1,34 @@
 import tkinter as tk
 from home import base_summary_frame
-from .graph import Graph
-from .cpu_data.global_cpu import GlobalCPU
+import psutil 
 
 
 class SummaryFrame(base_summary_frame.BaseSummaryFrame):
-    def __init__(self, master, logger, **options):
-        super().__init__(master, logger, **options)
-        self.cpu = GlobalCPU()
-        self.cpuUsageGraph = Graph.LineGraph(self)
+    def __init__(self, master, logger, name, **options):
+        super().__init__(master, logger, name, **options)
+        self.ramUsagePercent = None
+        self.ramUsageGB = None
+        self.totalRam = None
 
     def show(self):
-        self.cpuUsageGraph = Graph.LineGraph(self,width=40, padx=40, pady=40)
-        self.cpuUsageGraph.pack(fill=tk.BOTH)
-        self.cpuUsageGraph.show()
-        super().show()
+        self.totalRam = tk.Label(self, text="Total available : "+str(round((psutil.virtual_memory().total/1000000000),1))+"GB")
+        self.totalRam.pack(pady=10,padx=10)
+
+        self.ramUsagePercent = tk.Label(self, text="Used %")
+        self.ramUsagePercent.pack(pady=10,padx=10)
+
+        self.ramUsageGB = tk.Label(self, text="Used GB")
+        self.ramUsageGB.pack(pady=10,padx=10)
+
+        # super().show()
 
     def hide(self):
-        self.cpuUsageGraph.destroy()
+        self.totalRam.destroy()
+        self.ramUsageGB.destroy()
+        self.ramUsagePercent.destroy()
         super().hide()
 
     def update(self, dt):
-        self.cpu.update()
-        self.cpuUsageGraph.redraw(self.cpu.usages)
+        self.ramUsagePercent.config(text=(str(100-round((psutil.virtual_memory().available * 100 / psutil.virtual_memory().total),1))+'% used'))
+        self.ramUsageGB.config(text=(str(round((psutil.virtual_memory().total-psutil.virtual_memory().available)/1000000000,1))+'GB/'+
+        str(round((psutil.virtual_memory().total/1000000000),1))+"GB"))
