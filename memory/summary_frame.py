470b60d0bc844b20ef08f1f727b319be7559d137
Maxtho <maxime.pizzolitto82@gmail.com>
2022-02-27 16:38:12 +0100

    summary frame memory

--- memory/summary_frame.py
+++ memory/summary_frame.py
@@ -1,25 +1,34 @@
-import tkinter as tk
-from home import base_summary_frame
-from .graph import Graph
-from .cpu_data.global_cpu import GlobalCPU
+from tkinter import tk
+from home import mini_frame
+import psutil 
 
 
-class SummaryFrame(base_summary_frame.BaseSummaryFrame):
+class SummaryFrame(mini_frame.MiniFrame):
     def __init__(self, master, logger, **options):
         super().__init__(master, logger, **options)
-        self.cpu = GlobalCPU()
-        self.cpuUsageGraph = Graph.LineGraph(self)
+        self.ramUsagePercent = None
+        self.ramUsageGB = None
+        self.totalRam = None
 
     def show(self):
-        self.cpuUsageGraph = Graph.LineGraph(self,width=40, padx=40, pady=40)
-        self.cpuUsageGraph.pack(fill=tk.BOTH)
-        self.cpuUsageGraph.show()
+        self.totalRam = tk.Label(self.frame, text="Total available : "+str(round((psutil.virtual_memory().total/1000000000),1))+"GB")
+        self.totalRam.pack(pady=10,padx=10)
+
+        self.ramUsagePercent = tk.Label(self, text="Used %")
+        self.ramUsagePercent.pack(pady=10,padx=10)
+
+        self.ramUsageGB = tk.Label(self, text="Used GB")
+        self.ramUsageGB.pack(pady=10,padx=10)
+
         super().show()
 
     def hide(self):
-        self.cpuUsageGraph.destroy()
+        self.totalRam.destroy()
+        self.ramUsageGB.destroy()
+        self.ramUsagePercent.destroy()
         super().hide()
 
-    def update(self, dt):
-        self.cpu.update()
-        self.cpuUsageGraph.redraw(self.cpu.usages)
+    def update(self):
+        self.ramUsagePercent.config(text=(str(100-round((psutil.virtual_memory().available * 100 / psutil.virtual_memory().total),1))+'% used'))
+        self.ramUsageGB.config(text=(str(round((psutil.virtual_memory().total-psutil.virtual_memory().available)/1000000000,1))+'GB/'+
+        str(round((psutil.virtual_memory().total/1000000000),1))+"GB"))
